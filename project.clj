c8a915c75ee9b4a3834e0323c0e63acd546cb866
dom <ambient.crusader@gmail.com>
2012-11-06 15:05:51 -0600

    correcting project name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject google_api "0.1.0"
+(defproject google-api "0.1.0"
   :description "Google api library for clojure"
-  :url "github.com/dcar"
+  :url "https://github.com/dcar/google-api"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.5.6"]
                  [cheshire "4.0.3"]])
